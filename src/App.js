1916704d010bebe2d88a222d60a2d98b7e8eefdc
kumar sunny <coolsunny2099@gmail.com>
2023-10-13 14:58:13 +0530

    all done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Homepage from './component/Homepage';
+
+function App() {
+  return (
+    <div className="">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
